967b19d9478ad039cdbc97433fd9ad700221f778
Edoardo Panfili <edoardo.panfili@iisgubbio.edu.it>
2025-09-05 10:52:20 +0200

    patrizio round 9

--- sorgenti/script/test.js
+++ sorgenti/script/test.js
@@ -1,23 +1,88 @@
 function giusta(obj){
-        obj.style.backgroundColor="#88ff88";
-        obj.className = obj.className + " spiega";
+        obj.classList.add("spiega");
+        obj.classList.add("rispostaGiusta");
 }
 function sbagliata(obj){
-        obj.style.backgroundColor="#ff0000";
-        obj.className = obj.className + " spiega";
+        obj.classList.add("spiega");
+        obj.classList.add("rispostaSbagliata");
 }
-function sistemaIde(){
-	var nome=localStorage.getItem("ide");
-	var altri={"eclipse":".netbeans", "netbeans":".eclipse"};
-	if(nome!=null){
-		console.log(nome);
-		console.log("##"+altri[nome]);
-		elementList = document.querySelectorAll(altri[nome]);
-		for(i=0; i<elementList.length; i++){
-			console.log("  modificato "+elementList[i]);
-			elementList[i].className += " IDE_inutile";	
-		}
-	}else{
-		console.log("nessuna prefernza sull'IDE");
-	}
+
+/****************************************************************************
+ * Aggiunge un nodo come successivo a quello fornito
+ * @param {Node} nodoPresente 
+ * @param {Node} nodoDaAggiungere 
+ ***************************************************************************/
+function aggiungiSotto(nodoPresente, nodoDaAggiungere){
+    if(nodoPresente.nextSibling){
+        nodoPresente.parentNode.insertBefore(nodoDaAggiungere,nodoPresente.nextSibling);
+    }else{
+        nodoPresente.parentNode.appendChild(txt);
+    }
 }
+
+/***************************************************************************
+ * Controlla le risposte inserite dalll'utente nel test
+ * @param {Node} nodo 
+ * @param {Node} areaRisposta 
+ **************************************************************************/
+function controllaRisposte(nodo, areaRisposta){
+    var totali = 0;
+    var giuste = 0;
+    // elimino tutte le vecchie risposte
+    var vecchie = nodo.querySelectorAll("em");
+    for (var i=0;i<vecchie.length;i++){
+        nodo.removeChild(vecchie[i]);
+    }
+    var pezzi = nodo.querySelectorAll("span");
+    for (var i=0;i<pezzi.length;i++){
+        var giusta = pezzi[i].getAttribute("data-giusta").trim();
+        var data = pezzi[i].innerHTML.trim();
+        totali++;
+        if(giusta==data){
+            giuste++;
+            pezzi[i].className="giusta";
+        }else{
+            pezzi[i].className="sbagliata";
+            if(areaRisposta==null){
+                var g = document.createElement("em");
+                g.innerHTML=giusta;
+                aggiungiSotto(pezzi[i], g);
+            }
+        }
+    }
+    if(areaRisposta!=null){
+        areaRisposta.innerHTML="risposte esatte "+giuste+" su "+totali;
+    }
+}
+
+/****************************************************************************
+ * Analizza l'intero documento per sistemare i test con caselle da riempire
+ ***************************************************************************/
+function analizzaDocumento(){
+    var d = document.querySelectorAll(".riempiSpazi");
+    for(var x=0; x<d.length; x++){
+        var nodo = d[x];
+        var pezzi = nodo.querySelectorAll("span");
+        for (var i=0;i<pezzi.length;i++){
+            pezzi[i].setAttribute("data-giusta",pezzi[i].innerHTML);
+            pezzi[i].setAttribute("contenteditable","true");
+            pezzi[i].style.height="1.25em";
+            pezzi[i].style.width=pezzi[i].offsetWidth*1.4+"px";
+            pezzi[i].innerHTML = "";
+        }
+
+        var nc = document.createElement("div");
+        nc.innerHTML="mostra soluzione";
+        nc.className="mostraSoluzione";
+        nc.addEventListener("click",controllaRisposte.bind(null,nodo,null));
+        aggiungiSotto(nodo, nc);
+        
+        var elemento = document.createElement("div");
+        elemento.innerHTML="click qui per controllare la risposta";
+        elemento.className="controllaRisposta";
+        elemento.addEventListener("click",controllaRisposte.bind(null,nodo,elemento));
+        aggiungiSotto(nodo, elemento);
+    }
+}
+
+window.addEventListener('load', analizzaDocumento);
